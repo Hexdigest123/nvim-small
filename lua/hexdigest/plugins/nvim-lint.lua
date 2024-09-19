c98590cde75c86dcbb630f1929601f66c024c102
Pierre-Maurice Merckel <pierre@merckel.dev>
2024-09-19 16:55:44 +0200

    Changed autocmd events in nvim-lint

--- lua/hexdigest/plugins/nvim-lint.lua
+++ lua/hexdigest/plugins/nvim-lint.lua
@@ -7,7 +7,7 @@ return {
       cpp = { "cpplint" },
     }
 
-    vim.api.nvim_create_autocmd({ "BufWritePost" }, {
+    vim.api.nvim_create_autocmd({ "BufEnter", "BufWritePost", "InsertLeave" }, {
       callback = function()
         require("lint").try_lint()
       end,
