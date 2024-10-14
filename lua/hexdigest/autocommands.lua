b48585385fafd264fff2c42d9bfdce684494d298
Pierre-Maurice Merckel <pierre@merckel.dev>
2024-10-15 00:13:34 +0200

    Added autocommand support too run application via compiler.nvim

--- lua/hexdigest/autocommands.lua
+++ lua/hexdigest/autocommands.lua
@@ -161,7 +161,6 @@ vim.api.nvim_create_autocmd("BufRead", {
     vim.api.nvim_buf_create_user_command(0, "CreateJavaProject", function()
       local projectName = vim.fn.input("Enter Project Name: ")
       local packageName = vim.fn.input("Enter Package Name: ")
-
       local result = vim.fn.system(
         "mvn archetype:generate -DgroupId="
         .. packageName
@@ -169,6 +168,38 @@ vim.api.nvim_create_autocmd("BufRead", {
         .. projectName
         .. " -DarchetypeArtifactId=maven-archetype-quickstart -DinteractiveMode=false"
       )
+
+      if result == nil then
+        print("mvn generate failed!")
+        return nil
+      end
+
+      local current_dir = vim.fn.getcwd()
+      local file = io.open(current_dir .. "/" .. projectName .. "/" .. "Makefile", "w")
+      if file then
+        file:write(
+          "all: \n"
+          .. "\tmvn install\n"
+          .. "\tjava -cp target/classes "
+          .. packageName
+          .. ".App\n"
+          .. "verify: \n"
+          .. "\tmvn verify\n"
+          .. "validate: \n"
+          .. "\tmvn validate\n"
+          .. "package: \n"
+          .. "\tmvn package\n"
+          .. "test: \n"
+          .. "\tmvn test\n"
+        )
+        file:close()
+      else
+        print("Failed to create Makefile")
+      end
+      vim.fn.chdir(current_dir .. "/" .. projectName)
+      local oil = require("oil")
+      oil.close()
+      oil.open(vim.fn.getcwd())
       print(result)
     end, { desc = "Create a new Java project" })
   end,
