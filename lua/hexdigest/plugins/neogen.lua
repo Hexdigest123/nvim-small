d1a7c22fa9454df51d4ae59228c5076b9623bf06
Hexdigest123 <pierre@merckel.dev>
2025-03-08 16:38:22 +0100

    FIX: Neofetch not starting

--- lua/hexdigest/plugins/neogen.lua
+++ lua/hexdigest/plugins/neogen.lua
@@ -1,35 +1,38 @@
 return {
-	"danymat/neogen",
-	config = true,
-	keys = {
-		{
-			"<leader>nc",
-			function()
-				require("neogen").generate({
-					type = "class",
-				})
-			end,
-			desc = "Generate class annotation",
-		},
-		{
-			"<leader>nf",
-			function()
-				require("neogen").generate({
-					type = "func",
-				})
-			end,
-			desc = "Generate function annotation",
-		},
-		{
-			"<leader>nt",
-			function()
-				require("neogen").generate({
-					type = "type",
-				})
-			end,
-			desc = "Generate type annotation",
-		},
-	},
-	-- Uncomment next line if you want to follow only stable versions
-	-- version = "*"
+  "danymat/neogen",
+  lazy = false,
+  config = function()
+    require("neogen").setup({ snippet_engine = "luasnip" })
+  end,
+  keys = {
+    {
+      "<leader>nc",
+      function()
+        require("neogen").generate({
+          type = "class",
+        })
+      end,
+      desc = "Generate class annotation",
+    },
+    {
+      "<leader>nf",
+      function()
+        require("neogen").generate({
+          type = "func",
+        })
+      end,
+      desc = "Generate function annotation",
+    },
+    {
+      "<leader>nt",
+      function()
+        require("neogen").generate({
+          type = "type",
+        })
+      end,
+      desc = "Generate type annotation",
+    },
+  },
+  -- Uncomment next line if you want to follow only stable versions
+  -- version = "*"
 }
