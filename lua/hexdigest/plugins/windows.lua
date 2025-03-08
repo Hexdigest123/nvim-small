a54044d24cb3eb57c75a35f17a8f71cdf9e876bf
Hexdigest123 <pierre@merckel.dev>
2025-03-08 16:55:12 +0100

    ADD: Keybinds for windows.lua and indentation lines

--- lua/hexdigest/plugins/windows.lua
+++ lua/hexdigest/plugins/windows.lua
@@ -1,13 +1,15 @@
 return {
-	"anuvyklack/windows.nvim",
-	dependencies = {
-		"anuvyklack/middleclass",
-		"anuvyklack/animation.nvim",
-	},
-	config = function()
-		vim.o.winwidth = 10
-		vim.o.winminwidth = 10
-		vim.o.equalalways = false
-		require("windows").setup()
-	end,
+  "anuvyklack/windows.nvim",
+  dependencies = {
+    "anuvyklack/middleclass",
+    "anuvyklack/animation.nvim",
+  },
+  config = function()
+    vim.o.winwidth = 10
+    vim.o.winminwidth = 10
+    vim.o.equalalways = false
+    require("windows").setup()
+    vim.keymap.set("n", "<leader>wm", "<cmd>WindowsMaximize<cr>", { desc = "Maximize window" })
+    vim.keymap.set("n", "<leader>we", "<cmd>WindowsEqualize<cr>", { desc = "Equalize windows" })
+  end,
 }
